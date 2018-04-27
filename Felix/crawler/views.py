8e63ede460a47be62a9f76d8c0dd7f74bba742bc
Felix <wy47@duke.edu>
2018-04-27 18:46:35 -0400

    vulnerable to SllError

--- Felix/crawler/views.py
+++ Felix/crawler/views.py
@@ -9,40 +9,47 @@ from urllib.parse import urlparse
 def index(request):
     return render(request, 'index.html')
 
-def capture(url, label, attr, pool):
-    '''
-    capture new urls located at **attributes** of **labels** from **source urls**, 
-    grow **pool of target urls** and return it.
-    '''
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html.parser')
-    url_obj = urlparse(url)
-    for node in soup.find_all(label):
-        new_url = node.get(attr)
-        if new_url: # sometimes it's none
-            if new_url.startswith(url_obj.path): # relative path
-                new_url = url_obj.scheme + '://' + url_obj.netloc + new_url
-            if new_url.startswith(url_obj.scheme):
-                pool.append(new_url)
-    return pool
-
-def search(depth, imgs, targets, visited):
-    try:
-        togo = targets.pop()
-    except KeyError:
-        return imgs
-    imgs = capture(togo, 'img', 'src', imgs)
-    visited.add(togo)
-    if depth==1:
-        return imgs
-    else:
-        targets = capture(togo, 'a', 'href', targets)
-        return search(depth-1, imgs, targets, visited)
-    
-
 def crawl(request):
-    imgs = set(search(int(request.POST['depth']), [], [request.POST['start']], set()))   
+    
+    start = request.POST['start']
+    depth = int(request.POST['depth'])
+    
+    imgs=set()
+    targets=set([start])
+    visited=set()
+    
+    def capture(url, label, attr):
+        '''
+        capture new urls located at **attributes** of **labels** from **source urls**, 
+        '''
+        pool = []
+        page = requests.get(url).text
+        soup = BeautifulSoup(page, 'html.parser')
+        url_obj = urlparse(url)
+        for node in soup.find_all(label):
+            new_url = node.get(attr)
+            if new_url: # sometimes it's none
+                if new_url.startswith(url_obj.path): # relative path
+                    new_url = url_obj.scheme + '://' + url_obj.netloc + new_url
+                if new_url.startswith(url_obj.scheme):
+                    pool.append(new_url)
+        return pool
+
+    def search(depth):
+        while len(targets)>0:
+            togo = targets.pop()
+            if togo in visited:
+                break
+            print(togo)
+            imgs.update(capture(togo, 'img', 'src'))
+            visited.add(togo)
+        if depth!=1:
+            targets.update(capture(togo, 'a', 'href'))
+            return search(depth-1)
+        else:
+            return imgs
+
     context = {
-        'imgs':imgs,
+        'imgs':search(depth),
     }
     return render(request, 'result.html', context)
